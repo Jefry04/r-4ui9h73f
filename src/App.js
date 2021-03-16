2374afccbde46f513232a4e4516cfe863b776ac1
Jefferson <jefry04@gmail.com>
2021-03-16 11:05:15 -0500

    ready

--- src/App.js
+++ src/App.js
@@ -2,12 +2,28 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      text: ' ',
+    };
+  }
+  repeatText = (e) => {
+    this.setState({
+      text: e.target.value,
+    });
+  };
   render() {
     return (
       <div>
         <h1>Un repetidor</h1>
-        <input type="text" placeholder="Empieza a escribir algo" />
-        <p className="repeater"></p>
+        <input
+          type="text"
+          value={this.state.text}
+          onChange={this.repeatText.bind(this)}
+          placeholder="Empieza a escribir algo"
+        />
+        <p className="repeater">{this.state.text}</p>
       </div>
     );
   }
